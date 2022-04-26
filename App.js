d3baa8872188ea01375dbde30cbce20402f87910
josemucv2 <martinezcaceresjosem@gmail.com>
2022-04-25 20:31:47 -0400

    vue

---
+++ App.js
@@ -0,0 +1,23 @@
+var app = new Vue({
+    el: '#App',
+    data: {
+        message: 'Hello Vue!',
+        numeroA : 35,
+        numeroB : 5,
+    },
+    methods: {
+        calculoMatematico(){
+            return this.numeroA + this.numeroB
+        }
+
+    },
+
+    computed:{
+        duplicar(){
+            return this.numeroA * 2
+        }
+    }
+
+})
+
+
