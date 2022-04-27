f12a8e6ab1a2e8f8ebd23fc299bc19b745e08c1a
josemucv2 <martinezcaceresjosem@gmail.com>
2022-04-26 23:40:02 -0400

    Entrega Contador

--- App.js
+++ App.js
@@ -4,13 +4,28 @@ var app = new Vue({
         message: 'Hello Vue!',
         numeroA : 35,
         numeroB : 5,
+        contador :0,
     },
+    
     methods: {
+        
         calculoMatematico(){
             return this.numeroA + this.numeroB
+        },
+        
+        contadorsuma(){
+           this.contador++
+        },
+        
+        contadorResta(){ 
+            if(this.contador >0) {
+                this.contador--
+            } else {
+                alert('no se aceptan numeros negativos')
+            }
         }
-
     },
+    
     computed:{
         duplicar(){
             return this.numeroA * 2
